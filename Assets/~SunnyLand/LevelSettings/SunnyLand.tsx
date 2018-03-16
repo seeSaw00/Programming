c297a49961143df002cb6932009e62d2ff6c6561
edwin.mills <edwin.mills@600900J211DE404>
2018-03-16 11:49:44 +1100

    Started new game "SunnyLand"

---
+++ Assets/~SunnyLand/LevelSettings/SunnyLand.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SunnyLand" tilewidth="16" tileheight="16" tilecount="575" columns="25">
+ <image source="../Source/Repos/Programming/Assets/~SunnyLand/Environment/tileset.png" width="400" height="368"/>
+ <tile id="180" probability="0.1"/>
+ <tile id="182" probability="0.1"/>
+ <tile id="417" probability="0.1"/>
+ <tile id="418" probability="0.1"/>
+ <tile id="419" probability="0.1"/>
+ <tile id="469" probability="0.1"/>
+</tileset>
