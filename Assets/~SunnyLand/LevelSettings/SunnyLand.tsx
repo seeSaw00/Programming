f3e7d7f337994d4691ebcc450e0038e9304d555f
edwin.mills <edwin.mills@600900J211DE404>
2018-04-06 12:03:09 +1000

    Started implementing character controller

--- Assets/~SunnyLand/LevelSettings/SunnyLand.tsx
+++ Assets/~SunnyLand/LevelSettings/SunnyLand.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="SunnyLand" tilewidth="16" tileheight="16" tilecount="575" columns="25">
- <image source="../Source/Repos/Programming/Assets/~SunnyLand/Environment/tileset.png" width="400" height="368"/>
+ <image source="../Environment/tileset.png" width="400" height="368"/>
  <tile id="180" probability="0.1"/>
  <tile id="182" probability="0.1"/>
  <tile id="417" probability="0.1"/>
---
+++ Assets/~SunnyLand/Scripts/UserInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SunnyLand
+{
+    // Force Unity to attach "PlayerController"
+    [RequireComponent(typeof(PlayerController))]
+    public class UserInput : MonoBehaviour
+    {
+        public bool isJumping = false;
+        public bool isCrouching = false;
+        public float inputH, inputV;
+        
+        // Reference to player controller
+        private PlayerController player;
+
+        #region Unity Functions
+        // Use this for initialization
+        void Start()
+        {
+            player = GetComponent<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Update input
+            GetInput();
+            // Control the Player with input
+            player.Move(inputH);
+            player.Climb(inputV);
+            if (isJumping)
+            {
+                player.Jump();
+            }
+            if(isCrouching)
+            {
+                player.Crouch();
+                
+            }
+            else
+            {
+                player.UnCrouch();
+            }
+        }
+        #endregion
+
+        #region Custom Functions
+        void GetInput()
+        {
+            inputH = Input.GetAxisRaw("Horizontal");
+            inputV = Input.GetAxisRaw("Vertical");
+            isJumping = Input.GetKeyDown(KeyCode.Space);
+            isCrouching = Input.GetKey(KeyCode.LeftControl);
+        }
+        #endregion
+    }
+}
